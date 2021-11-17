9d4ff941edac539f2a6916998d1501055bab1b1a
dfahlander <david.fahlander@gmail.com>
2021-11-17 23:20:40 +0100

    m

--- publish.sh
+++ publish.sh
@@ -2,3 +2,8 @@
 git fetch upstream gh-pages
 git merge upstream/gh-pages -m merge
 git push
+
+
+# NOTE: To publish a certain branch from upstream:
+# git pull upstream autumn-2021-release
+# git push
